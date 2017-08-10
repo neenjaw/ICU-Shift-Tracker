a6a646d8061e8a0635deec429bea0f86c370126c
neenjaw <tim@neenjaw.com>
2017-08-10 02:49:05 -0600

    Revamped col/row Changed radio button style Responsive, mobile friendlier

--- add-shift.php
+++ add-shift.php
@@ -30,73 +30,122 @@ if (!isset($_SESSION['user_session'])) {
     <div class="col-12 col-md-auto">
       <br>
       <!-- Main Content -->
-      <h2>Add a new Shift</h2>
 
-      <form method="post" id="shift-form">
-        <div class="form-group">
-          <label class="control-label requiredField" for="select">Select a Staff for the shift:<span class="asteriskField">*</span></label>
-          <select class="select form-control" id="select-staff" name="select">
-            <option value="staff_id">Last, Name - RN</option>
-          </select>
-        </div>
+      <div class="container">
+        <form method="post" id="shift-form">
+          <div class="row justify-content-center">
+            <div class="col-sm-8">
 
-        <div class="form-group">
-          <label class="control-label requiredField" for="date">Date<span class="asteriskField">*</span></label>
-          <div class="input-group">
-            <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-            <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
-          </div>
-        </div>
+              <h2>Add a new Shift</h2>
+              <hr />
 
-        <div class="form-group">
-          <label class="control-label requiredField">Day or Night<span class="asteriskField">*</span></label>
-          <div class="">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="radio-d-or-n" id="radio-d-or-n-1" value="D"/>
-                Day
-              </label>
-            </div>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="radio-d-or-n" id="radio-d-or-n-2" value="N"/>
-                Night
-              </label>
             </div>
           </div>
-        </div>
+          <div class="row justify-content-center">
 
-        <div class="form-group">
-          <label class="control-label requiredField" for="select1">Role<span class="asteriskField">*</span></label>
-          <select class="select form-control" id="select1" name="select1">
-            <option value="First Choice">First Choice</option>
-            <option value="Second Choice">Second Choice</option>
-            <option value="Third Choice">Third Choice</option>
-          </select>
-        </div>
+            <div class="col-sm-8 form-control-feedback hidden" id="shift-form-feedback"></div>
 
-        <div class="form-group">
-          <label class="control-label requiredField" for="select2">Pod Assignment<span class="asteriskField">*</span></label>
-          <select class="select form-control" id="select2" name="select2">
-            <option value="First Choice">First Choice</option>
-            <option value="Second Choice">Second Choice</option>
-            <option value="Third Choice">Third Choice</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label class="control-label ">Check all that apply</label>
-          <div class=" ">
-            <div class="checkbox"><label class="checkbox"><nput name="checkbox" type="checkbox" value="Non-vented?"/>Non-vented?</label></div>
-            <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Doubled?"/>Doubled?</label></div>
-            <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Admitted?"/>Admitted?</label></div>
-            <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Code pager?"/>Code pager?</label></div>
-            <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Very sick?"/>Very sick?</label></div>
           </div>
-        </div>
-        <div class="form-group">
-          <div><button class="btn btn-primary " name="submit" type="submit">Submit</button></div>
-        </div>
-      </form>
+
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+              <label class="control-label requiredField" for="select">Select a Staff for the shift:<span class="asteriskField">*</span></label>
+              <select class="select form-control" id="select-staff" name="select">
+                <option value="staff_id">Last, Name - RN</option>
+              </select>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+              <label class="control-label requiredField" for="date">Date<span class="asteriskField">*</span></label>
+              <div class="input-group">
+                <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+                <input class="form-control" id="date" name="date" placeholder="MM/DD/YYYY" type="text"/>
+              </div>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 btn-group requiredField" data-toggle="buttons">
+              <label class="btn btn-outline-primary active">
+                <input type="radio" name="radio-d-or-n" id="radio-d-or-n-d" value="D" autocomplete="off" checked> Day Shift
+              </label>
+              <label class="btn btn-outline-primary">
+                <input type="radio" name="radio-d-or-n" id="radio-d-or-n-n" value="N" autocomplete="off"> Night Shift
+              </label>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+              <label class="control-label requiredField" for="select1">Role<span class="asteriskField">*</span></label>
+              <select class="select form-control" id="select1" name="select1">
+                <option value="First Choice">First Choice</option>
+                <option value="Second Choice">Second Choice</option>
+                <option value="Third Choice">Third Choice</option>
+              </select>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+              <label class="control-label requiredField" for="select2">Pod Assignment<span class="asteriskField">*</span></label>
+              <select class="select form-control" id="select2" name="select2">
+                <option value="First Choice">First Choice</option>
+                <option value="Second Choice">Second Choice</option>
+                <option value="Third Choice">Third Choice</option>
+              </select>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+              <label class="control-label ">Check all that apply</label>
+              <div class=" ">
+                <div class="checkbox"><label class="checkbox"><nput name="checkbox" type="checkbox" value="Non-vented?"/>Non-vented?</label></div>
+                <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Doubled?"/>Doubled?</label></div>
+                <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Admitted?"/>Admitted?</label></div>
+                <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Code pager?"/>Code pager?</label></div>
+                <div class="checkbox"><label class="checkbox"><input name="checkbox" type="checkbox" value="Very sick?"/>Very sick?</label></div>
+              </div>
+            </div>
+
+          </div>
+          <div class="row justify-content-center">
+
+            <div class="col-sm-8 form-group">
+            </div>
+
+          </div>
+
+
+
+          <div class="row justify-content-center">
+
+            <div class="col-sm-4 form-group">
+            </div>
+
+            <div class="col-sm-4 form-group">
+            </div>
+
+          </div>
+          <div class="row justify-content-end">
+
+            <div class="col-sm-4 form-group">
+              <div><button class="btn btn-primary " name="submit" type="submit">Submit</button></div>
+            </div>
+
+          </div>
+        </form>
+      </div>
+
 
     <!-- END Main Content -->
   </div>
