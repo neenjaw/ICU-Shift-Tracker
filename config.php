06bde0574e47cbe3fd30de88ee62fc37a4dd03ff
neenjaw <tim@neenjaw.com>
2017-10-20 00:03:18 -0600

    added checkbox enable/disable

--- config.php
+++ config.php
@@ -39,113 +39,113 @@ if (!isset($_SESSION['user'])) {
         <h2>Config</h2>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <div class="col">
-        <div id='cmd-options' class="btn-group mt-3 mb-3" role="group" aria-label="First group">
-          <button type="button" class="btn btn-primary" data-cmd="changepw">Change Password</button>
-          <button type="button" class="btn btn-secondary" data-cmd="addusr">Add a user</button>
-          <button type="button" class="btn btn-secondary" data-cmd="modusr">Modify User</button>
-          <button type="button" class="btn btn-secondary" data-cmd="delusr">Delete User</button>
+    <div class="row">
+      <div class="col-sm-3">
+        <div id='cmd-options' class="btn-group-vertical mt-3 mb-3" role="group" aria-label="First group">
+          <button type="button" class="btn btn-primary btn-block" data-cmd="changepw">Change Password</button>
+          <button type="button" class="btn btn-secondary btn-block" data-cmd="addusr">Add a user</button>
+          <button type="button" class="btn btn-secondary btn-block" data-cmd="modusr">Modify User</button>
+          <button type="button" class="btn btn-secondary btn-block" data-cmd="delusr">Delete User</button>
         </div>
       </div>
-    </div>
-    <div class="row">
-      <form id="cmd-form">
-        <div id="cmd-changepw" class="col form-section current">
-          <div class="form-group">
-            <label for="cmd-changepw-old-pw">Old Password</label>
-            <input type="password" class="form-control" id="cmd-changepw-old-pw" placeholder="Old Password" autocomplete="off">
+      <div class="col-sm-9">
+        <form id="cmd-form">
+          <div id="cmd-changepw" class="form-section current">
+            <div class="form-group">
+              <label for="cmd-changepw-old-pw">Old Password</label>
+              <input type="password" class="form-control" id="cmd-changepw-old-pw" placeholder="Old Password" autocomplete="off">
+            </div>
+            <div class="form-group">
+              <label for="cmd-changepw-new-pw">New Password</label>
+              <input type="password" class="form-control" id="cmd-changepw-new-pw" placeholder="New Password" autocomplete="off">
+            </div>
+            <div class="form-group">
+              <label for="cmd-changepw-rpt-pw">Repeat Password</label>
+              <input type="password" class="form-control" id="cmd-changepw-rpt-pw" placeholder="Repeat Password" autocomplete="off">
+            </div>
+            <button id="cmd-changepw-submit" type="submit" class="btn btn-primary">Change Password</button>
           </div>
-          <div class="form-group">
-            <label for="cmd-changepw-new-pw">New Password</label>
-            <input type="password" class="form-control" id="cmd-changepw-new-pw" placeholder="New Password" autocomplete="off">
-          </div>
-          <div class="form-group">
-            <label for="cmd-changepw-rpt-pw">Repeat Password</label>
-            <input type="password" class="form-control" id="cmd-changepw-rpt-pw" placeholder="Repeat Password" autocomplete="off">
-          </div>
-          <button id="cmd-changepw-submit" type="submit" class="btn btn-primary">Change Password</button>
-        </div>
-        <div id="cmd-addusr" class="col form-section">
-          <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
-            <p>You do are not an administrator of this page, contact an administrator to add users.</p>
-          <?php else: ?>
-            <div class="form-group">
-              <label for="cmd-addusr-new-username">New Username</label>
-              <input type="text" class="form-control" id="cmd-addusr-new-username" placeholder="Username">
-            </div>
-            <div class="form-group">
-              <label for="cmd-addusr-new-pw">New Password</label>
-              <input type="password" class="form-control" id="cmd-addusr-new-pw" placeholder="New Password" autocomplete="off">
-            </div>
-            <div class="form-group">
-              <label for="cmd-addusr-rpt-pw">Repeat Password</label>
-              <input type="password" class="form-control" id="cmd-addusr-rpt-pw" placeholder="Repeat Password" autocomplete="off">
-            </div>
-            <div class="form-group">
-              <label for="cmd-addusr-auth-id">App Authorization:</label>
-              <select class="form-control" id="cmd-addusr-auth-id">
-                <option selected disabled>loading...</option>
-              </select>
-            </div>
-            <button id="cmd-addusr-submit" type="submit" class="btn btn-primary">Add New User</button>
-          <?php endif; ?>
-        </div>
-        <div id="cmd-modusr" class="col form-section">
-          <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
-            <p>You do are not an administrator of this page, contact an administrator to modify users.</p>
-          <?php else: ?>
-            <div class="form-group">
-              <label for="cmd-modusr-uid">Select user to modify:</label>
-              <select class="form-control" id="cmd-modusr-uid">
-                <option selected disabled>loading...</option>
-              </select>
-            </div>
-            <div class="p-2 mb-2 border border-secondary">
-              <label class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="cmd-modusr-chk-pw">
-                <span class="custom-control-indicator"></span>
-                <span class="custom-control-description">Change this user's password?</span>
-              </label>
+          <div id="cmd-addusr" class="form-section">
+            <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
+              <p>You do are not an administrator of this page, contact an administrator to add users.</p>
+            <?php else: ?>
               <div class="form-group">
-                <label for="cmd-modusr-new-pw">New Password</label>
-                <input type="password" class="form-control" id="cmd-modusr-new-pw" placeholder="New Password" autocomplete="off"  disabled>
+                <label for="cmd-addusr-new-username">New Username</label>
+                <input type="text" class="form-control" id="cmd-addusr-new-username" placeholder="Username">
               </div>
               <div class="form-group">
-                <label for="cmd-modusr-rpt-pw">Repeat Password</label>
-                <input type="password" class="form-control" id="cmd-modusr-rpt-pw" placeholder="Repeat Password" autocomplete="off"  disabled>
+                <label for="cmd-addusr-new-pw">New Password</label>
+                <input type="password" class="form-control" id="cmd-addusr-new-pw" placeholder="New Password" autocomplete="off">
               </div>
-            </div>
-            <div class="p-2 mb-2 border border-secondary">
-              <label class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="cmd-modusr-chk-auth">
-                <span class="custom-control-indicator"></span>
-                <span class="custom-control-description">Change this user's app authorization?</span>
-              </label>
               <div class="form-group">
-                <label for="exampleFormControlSelect1">App Authorization:</label>
-                <select class="form-control" id="cmd-modusr-auth-id" disabled>
+                <label for="cmd-addusr-rpt-pw">Repeat Password</label>
+                <input type="password" class="form-control" id="cmd-addusr-rpt-pw" placeholder="Repeat Password" autocomplete="off">
+              </div>
+              <div class="form-group">
+                <label for="cmd-addusr-auth-id">App Authorization:</label>
+                <select class="form-control" id="cmd-addusr-auth-id">
                   <option selected disabled>loading...</option>
                 </select>
               </div>
-            </div>
-            <button id="cmd-modusr-submit" type="submit" class="btn btn-primary">Add New User</button>
-          <?php endif; ?>
-        </div>
-        <div id="cmd-delusr" class="col form-section">
-          <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
-            <p>You do are not an administrator of this page, contact an administrator to delete users.</p>
-          <?php else: ?><form>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1">Select user to delete:</label>
-              <select class="form-control" id="cmd-delusr-uid">
-                <option selected disabled>loading...</option>
-              </select>
-            </div>
-            <button id="cmd-delusr-submit" type="submit" class="btn btn-danger">Delete user</button>
-          <?php endif; ?>
-        </div>
-      </form>
+              <button id="cmd-addusr-submit" type="submit" class="btn btn-primary">Add New User</button>
+            <?php endif; ?>
+          </div>
+          <div id="cmd-modusr" class="form-section">
+            <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
+              <p>You do are not an administrator of this page, contact an administrator to modify users.</p>
+            <?php else: ?>
+              <div class="form-group">
+                <label for="cmd-modusr-uid">Select user to modify:</label>
+                <select class="form-control" id="cmd-modusr-uid">
+                  <option selected disabled>loading...</option>
+                </select>
+              </div>
+              <div class="p-2 mb-2 border border-secondary">
+                <label class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="cmd-modusr-chk-pw">
+                  <span class="custom-control-indicator"></span>
+                  <span class="custom-control-description">Change this user's password?</span>
+                </label>
+                <div class="form-group">
+                  <label for="cmd-modusr-new-pw">New Password</label>
+                  <input type="password" class="form-control" id="cmd-modusr-new-pw" placeholder="New Password" autocomplete="off"  disabled>
+                </div>
+                <div class="form-group">
+                  <label for="cmd-modusr-rpt-pw">Repeat Password</label>
+                  <input type="password" class="form-control" id="cmd-modusr-rpt-pw" placeholder="Repeat Password" autocomplete="off"  disabled>
+                </div>
+              </div>
+              <div class="p-2 mb-2 border border-secondary">
+                <label class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="cmd-modusr-chk-auth">
+                  <span class="custom-control-indicator"></span>
+                  <span class="custom-control-description">Change this user's app authorization?</span>
+                </label>
+                <div class="form-group">
+                  <label for="exampleFormControlSelect1">App Authorization:</label>
+                  <select class="form-control" id="cmd-modusr-auth-id" disabled>
+                    <option selected disabled>loading...</option>
+                  </select>
+                </div>
+              </div>
+              <button id="cmd-modusr-submit" type="submit" class="btn btn-primary">Add New User</button>
+            <?php endif; ?>
+          </div>
+          <div id="cmd-delusr" class="form-section">
+            <?php if ($_SESSION['user']->auth_state !== 'admin'): ?>
+              <p>You do are not an administrator of this page, contact an administrator to delete users.</p>
+            <?php else: ?><form>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Select user to delete:</label>
+                <select class="form-control" id="cmd-delusr-uid">
+                  <option selected disabled>loading...</option>
+                </select>
+              </div>
+              <button id="cmd-delusr-submit" type="submit" class="btn btn-danger">Delete user</button>
+            <?php endif; ?>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 
@@ -168,10 +168,10 @@ if (!isset($_SESSION['user'])) {
 
   <script>
   /*
-   * TODO mod chk enables form
-   * TODO Form submission
+   * TODO Form validation and submission
    *
-   * QUESTION if I can't set the global from the async function, THEN HOW?!?!?!?
+   * QUESTION if I can't set the global from the async function, THEN HOW?!?!?!? -->
+   * is why you need ES2017 JS or a framework that facilitates asynchronous
    */
 
 
@@ -179,9 +179,6 @@ if (!isset($_SESSION['user'])) {
 
   var optionTemplate;
   var optionLoading = [{value:'', text:'loading...'}];
-
-  var users;
-  var authStates;
 
   $(function() {
     $(`#cmd-options`).children().click(function(){
@@ -193,72 +190,91 @@ if (!isset($_SESSION['user'])) {
       $(`#cmd-${cmd}`).addClass('current');
     });
 
+    $(`#cmd-modusr-chk-pw`).click(function(){
+      if ($(this).prop(`checked`)) {
+        $(`#cmd-modusr-new-pw`).prop(`disabled`, false);
+        $(`#cmd-modusr-rpt-pw`).prop(`disabled`, false);
+      } else {
+        $(`#cmd-modusr-new-pw`).prop(`disabled`, true);
+        $(`#cmd-modusr-rpt-pw`).prop(`disabled`, true);
+        $(`#cmd-modusr-new-pw`).prop(`value`, ``);
+        $(`#cmd-modusr-rpt-pw`).prop(`value`, ``);
+      }
+    });
+
+    $(`#cmd-modusr-chk-auth`).click(function(){
+      if ($(this).prop(`checked`)) {
+        $(`#cmd-modusr-auth-id`).prop(`disabled`, false);
+      } else {
+        $(`#cmd-modusr-auth-id`).prop(`disabled`, true);
+        $(`#cmd-modusr-auth-id`).prop(`value`, ``);
+      }
+    });
+
     optionTemplate = Handlebars.compile($("#select-option-template").html());
 
-    getAuthStates(optionTemplate, authStates, optionLoading);
-    getUsers(optionTemplate, users, optionLoading);
+    getAuthStates(optionTemplate, optionLoading);
+    getUsers(optionTemplate, optionLoading);
   });
 
-  function getAuthStates(passedTemplate, aList, defaultList) {
+  function getAuthStates(pTemplate, defaultList) {
     $.ajax({
       type: 'GET',
       url: 'ajax/ajax_get_auth_states.php',
       data: '',
       beforeSend: function () {
-        aList = defaultList;
-        setSelectToLoading($('#cmd-addusr-auth-id'), passedTemplate, {entry: aList});
-        setSelectToLoading($('#cmd-modusr-auth-id'), passedTemplate, {entry: aList});
+        setSelectToLoading($('#cmd-addusr-auth-id'), pTemplate, {entry: defaultList});
+        setSelectToLoading($('#cmd-modusr-auth-id'), pTemplate, {entry: defaultList});
       },
       success: function (response) {
-        aList = JSON.parse(response);
+        let al = JSON.parse(response);
         if (debug) console.log("AJAX returned, states list:");
-        if (debug) console.log(aList);
+        if (debug) console.log(al);
 
         //map authStates to appropriate format for makeSelect id->value, state->text
-        let mappedStates = $.map(aList, function(e, i) {
+        let mappedStates = $.map(al, function(e, i) {
                              return {value:e.id, text:e.state};
                            });
 
-        fillSelect($('#cmd-addusr-auth-id'), passedTemplate, {entry: mappedStates});
-        fillSelect($('#cmd-modusr-auth-id'), passedTemplate, {entry: mappedStates});
+        fillSelect($('#cmd-addusr-auth-id'), pTemplate, {entry: mappedStates});
+        fillSelect($('#cmd-modusr-auth-id'), pTemplate, {entry: mappedStates});
       }
     });
   }
 
-  function getUsers(passedTemplate, uList, defaultList) {
+  function getUsers(pTemplate, defaultList) {
     $.ajax({
       type: 'GET',
       url: 'ajax/ajax_get_users.php',
       data: '',
       beforeSend: function () {
-        uList = defaultList;
-        setSelectToLoading($('#cmd-delusr-uid'), passedTemplate, {entry: uList});
-        setSelectToLoading($('#cmd-modusr-uid'), passedTemplate, {entry: uList});
+        setSelectToLoading($('#cmd-delusr-uid'), pTemplate, {entry: defaultList});
+        setSelectToLoading($('#cmd-modusr-uid'), pTemplate, {entry: defaultList});
       },
       success: function (response) {
-        uList = JSON.parse(response);
+        let ul = JSON.parse(response);
         if (debug) console.log("AJAX returned, user list:");
-        if (debug) console.log(uList);
+        if (debug) console.log(ul);
 
         //map users to appropriate format for makeSelect id->value, login->text
-        let mappedUsers = $.map(uList, function(e, i) {
+        let mappedUsers = $.map(ul, function(e, i) {
                             return {value:e.id, text:e.login};
                           });
 
-        fillSelect($('#cmd-modusr-uid'), passedTemplate, {entry: mappedUsers});
-        fillSelect($('#cmd-delusr-uid'), passedTemplate, {entry: mappedUsers});
+        fillSelect($('#cmd-modusr-uid'), pTemplate, {entry: mappedUsers});
+        fillSelect($('#cmd-delusr-uid'), pTemplate, {entry: mappedUsers});
       }
     });
   }
 
-  function fillSelect($target, passedTemplate, entryList) {
+  function fillSelect($target, pTemplate, entryList) {
     $target.empty()
-           .append(passedTemplate(entryList));
+           .append(pTemplate(entryList));
   }
 
-  function setSelectToLoading($target, passedTemplate, entryList) {
+  function setSelectToLoading($target, pTemplate, entryList) {
     $target.empty()
-           .append(passedTemplate(entryList))
+           .append(pTemplate(entryList))
            .children()
            .first()
            .attr('disabled', true)
