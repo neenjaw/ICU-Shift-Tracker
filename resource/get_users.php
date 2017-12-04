e0c14293084b0a2c8bf8833fdeb1d9c2181f31cb
neenjaw <tim@neenjaw.com>
2017-12-03 23:33:43 -0700

    config bug fixes and form use issue fix

--- resource/get_users.php
+++ resource/get_users.php
@@ -10,19 +10,34 @@ if ($_SESSION['user']->auth_state !== 'admin') {
   die("Unauthorized. Must be an administrator.");
 }
 
-// $DB_con
-// $DB_table->users = $DB_tbl_users;
+if (isset($_GET['user'])) {
 
-$sql = "SELECT id, login FROM {$DB_table->users} WHERE login != '{$_SESSION['user']->login}'";
+  $sql = "SELECT auth_id FROM {$DB_table->users} WHERE id = :lid";
+  $stmt = $DB_con->prepare($sql);
+  $stmt->bindparam(":lid", $_GET['user'], PDO::PARAM_STR);
+  $stmt->execute();
 
-$stmt = $DB_con->prepare($sql);
-$stmt->execute();
+  if ($stmt->rowCount() < 1) {
+    echo "{\"error\":\"Unable to retrieve authorization level\"}";
+  } else {
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "{\"auth\":{$row['auth_id']}}";
+  }
 
-$result = array();
-while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-  array_push($result, $row);
+} else {
+
+  $sql = "SELECT id, login FROM {$DB_table->users} WHERE login != '{$_SESSION['user']->login}'";
+
+  $stmt = $DB_con->prepare($sql);
+  $stmt->execute();
+
+  $result = array();
+  while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+    array_push($result, $row);
+  }
+
+  echo json_encode($result);
+
 }
-
-echo json_encode($result);
 
 ?>
